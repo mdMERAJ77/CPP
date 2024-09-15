0f235574457b6005ded87497873188868849a97a
mdMERAJ77 <mmd200975@gmail.com>
2024-09-15 18:45:32 +0530

    Successfully Commit

---
+++ Apna College/Day 1/firstProgram.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"apna college"<<endl;
+	return 0;
+}
---
+++ Apna College/Day 1/variables.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int age=25;
+	char grade='A';
+	bool istrue=true;
+	float marks=9.9;
+	double salary=223434;
+	
+	
+	int size1=sizeof(age);
+	float size2=sizeof(marks);
+	char size3=sizeof(grade);
+	cout<<age<<"\n"<<grade<<"\n"<<istrue<<"\n"<<marks<<"\n"<<salary;
+	
+	
+	
+	
+	
+}
