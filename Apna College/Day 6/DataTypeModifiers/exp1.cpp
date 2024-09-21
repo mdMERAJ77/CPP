e945a26f53ff751b9647635cc705bffb3609fd6c
mdMERAJ77 <mmd200975@gmail.com>
2024-09-21 22:40:30 +0530

    Successfully Commit

--- Apna College/Day 6/DataTypeModifiers/exp1.cpp
+++ Apna College/Day 6/DataTypeModifiers/exp1.cpp
@@ -5,5 +5,17 @@ int main(){
 	//note system(pc) must be  64-bit architecture.
 	int x;
 	cout<<sizeof(x)<<endl;
-	cout<<sizeof(long (x))<<endl;
+	cout<<sizeof(long (x))<<endl; //not give accurate vlaue because compiler not support
+	
+	cout<<sizeof(short)<<endl;
+	int age=15;
+	cout<<sizeof(short(age))<<endl;
+	
+	cout<<sizeof(long long)<<endl;
+	
+	//difference between signed and unsigend
+	unsigned int num=-1;
+	int x1=num;
+	cout<<num<<", "<<x1;
+	return 0;
 }
