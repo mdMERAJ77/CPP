e3f439d7b739fbc3b9329b982acc1f16683960c2
mdMERAJ77 <mmd200975@gmail.com>
2024-09-22 20:52:36 +0530

    Successfully Commit

--- Apna College/Day 7/Arrays/reverseArr.cpp
+++ Apna College/Day 7/Arrays/reverseArr.cpp
@@ -1,25 +1,18 @@
 #include<iostream>
 using namespace std;
-
-void reverseArr(int arr[],int size){
+void revArr(int arr[], int size){
 	int start=0; int end=size-1;
-	
 	while(start<end){
 		swap(arr[start],arr[end]);
 		start++;
 		end--;
 	}
-	
 }
 int main(){
-	
-	int arr[]={1,2,3,4};
-	int size=4;
-	reverseArr( arr,4);
-	for(int i=0; i<size; i++){
+	int arr[]={1,2,3};
+	int size=3;
+	revArr(arr,size);
+	for(int i=0; i<size;i++){
 		cout<<arr[i]<<" ";
 	}
-	cout<<endl;
-	
-	return 0;
 }
