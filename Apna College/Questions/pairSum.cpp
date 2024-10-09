682a867f918055b19601af931c1edc4713932e5e
mdMERAJ77 <mmd200975@gmail.com>
2024-10-09 19:39:20 +0530

    Successfully Commit

--- Apna College/Questions/pairSum.cpp
+++ Apna College/Questions/pairSum.cpp
@@ -1,14 +1,32 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int main(){
-	int arr[]={2,7,8,15};
-	int target=9,n=4;
-	for(int i=0; i<n; i++){
-		for(int j=i+1; j<n-j; j++){
-			if(target == (arr[i]+arr[j])){
-				
-			}
+
+vector<int> pairSum(vector<int> v, int target){
+	vector<int> ans;
+	int n=v.size();
+	int i=0, j=n-1;
+	while(i<j){
+		int pairSum=v[i]+v[j];
+		if(pairSum>target){
+			j--;
+		}
+		else if(pairSum<target){
+			i++;
+		}
+		else{
+			ans.push_back(i);
+			ans.push_back(j);
+			return ans;
 		}
 	}
+}
+int main(){
+	vector<int>v={2,7,11,15};
+	int target=17;
+	
+	vector<int> ans=pairSum(v,target);
+	cout<<ans[0]<<", "<<ans[1]<<endl;
+	
 	return 0;
 }
---
+++ loveBabbar/vector/subarray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[5]={1,2,3};
+	int n=5;
+	int maxsub=INT_MIN;
+	int currsum=0;
+	for(int start=0; start<n; start++){
+		
+		for(int end=start; end<n; end++){
+			currsum=currsum+arr[end];
+			maxsub=max(currsum,maxsub);
+		}
+	}
+	cout<<maxsub;
+	
+	
+	return 0;
+}
