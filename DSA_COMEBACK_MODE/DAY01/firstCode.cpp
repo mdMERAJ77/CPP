fcf9da1a808ae76198cda59a917e6a855e5af311
mdMERAJ77 <mmd200975@gmail.com>
2025-10-23 21:33:55 +0530

    learn about variables and datatypes

--- DSA_COMEBACK_MODE/DAY01/firstCode.cpp
+++ DSA_COMEBACK_MODE/DAY01/firstCode.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 int main(){
 	//varialbe declare
-	int a;
+	int a; //4bytes
 	//now value assign in a
 	a=10;
-	cout<<"number "<<a;
+	cout<<"number "<<a<<endl;
+	//char
+	char c='a'; //1bytes
+	cout<<"char "<<c<<endl;
+	//float
+	float f=2.31; //4bytes
+	cout<<"float"<<f<<endl;
+	
+	//double
+	double d=3.23434234234;
+	cout<<"double"<<d<<endl;
+	
 	return 0;
 	
 }
