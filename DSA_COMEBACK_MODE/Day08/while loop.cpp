1c2810b302ed97480b7646310d2e9b9159217f1c
mdMERAJ77 <mmd200975@gmail.com>
2025-10-29 22:49:19 +0530

    while loop

---
+++ DSA_COMEBACK_MODE/Day08/while loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//initialization
+	//break condition
+	//updation
+	int n;
+	cout<<"enter a number: ";
+	cin>>n;
+	int i=1; // initialization
+	while(i<=10){ 	//break condition
+		cout<<n*i<<endl;
+		i++; //updation	
+	}
+	return 0;
+}
