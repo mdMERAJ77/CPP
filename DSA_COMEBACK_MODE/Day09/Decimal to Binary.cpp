3f559e4bf941745bbd3bdceb1f99f35d0fc32008
mdMERAJ77 <mmd200975@gmail.com>
2025-11-03 22:57:10 +0530

    Decimal to Binary Conversion

--- DSA_COMEBACK_MODE/Day09/Decimal to Binary.cpp
+++ DSA_COMEBACK_MODE/Day09/Decimal to Binary.cpp
@@ -6,8 +6,13 @@ int main(){
 	cin>>num;
 	int rem,ans=0,mul=1;
 	while(num>0){
+		//remainder
 		rem=num%2; //we also use bitwise AND: num&1
+		
+		//quotient
 		num=num/2; //we also use right shift operator: num>>1 
+		
+		//logic to convert Decimal to Binary form
 		ans=rem*mul+ans;
 		mul=mul*10;
 	}
