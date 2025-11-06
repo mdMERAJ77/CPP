25bacf593ab86561ecf6e04fd71e1e6dda677897
mdMERAJ77 <mmd200975@gmail.com>
2025-11-06 23:23:48 +0530

    Reverse the number

---
+++ DSA_COMEBACK_MODE/Day10/digitReverse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num,rem,ans=0;
+	cout<<"enter a number: ";
+	cin>>num;
+	while(num>0){
+		rem=num%10;
+		num=num/10;
+		ans=ans*10+rem;
+	}
+	cout<<"is reverse number: "<<ans;
+	return 0;
+}
