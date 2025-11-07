6d7a3c10ea95346f0fe63b73ae33962a0630249e
mdMERAJ77 <mmd200975@gmail.com>
2025-11-07 11:04:01 +0530

    leetcode7_integerReverse

--- DSA_COMEBACK_MODE/Day10/leetcode7_integerReverse.cpp
+++ DSA_COMEBACK_MODE/Day10/leetcode7_integerReverse.cpp
@@ -7,6 +7,12 @@ int main(){
 	while(num!=0){
 		rem=num%10;
 		num=num/10;
+		//if you have 32 bit integer then how to handle integer overflow:
+		//1: ans>INT_MAX/10
+		//2: ans<INT_MIN/10
+		if(ans>INT_MAX || ans<INT_MIN){
+			return 0;
+		}
 		ans=ans*10+rem;
 	}
 	cout<<"is reverse number: "<<ans;
