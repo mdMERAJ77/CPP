1f681624f53f612aabcbb09d19d0bb73e9016f85
mdMERAJ77 <mmd200975@gmail.com>
2025-11-08 11:49:05 +0530

    complement of Number

---
+++ DSA_COMEBACK_MODE/Day10/complentOfNumber.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+	int num,ans=0,rem,mul=1;
+	cout<<"enter a number: ";
+	cin>>num;
+	while(num>0){
+		rem=num%2;
+		if(rem==1){
+			rem=0*mul;
+		}
+		else{
+			rem=1*mul;
+		}
+		num=num/2;
+		ans=ans+rem;
+		mul=mul*2;
+	}
+	cout<<ans;
+	return 0;
+}
