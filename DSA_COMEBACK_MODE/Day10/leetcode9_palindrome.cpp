fb670c1fdadd093888a0f66bfbb0f508e6415fdf
mdMERAJ77 <mmd200975@gmail.com>
2025-11-08 10:38:24 +0530

    leetcode 9 palindrome

---
+++ DSA_COMEBACK_MODE/Day10/leetcode9_palindrome.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num,ans=0,copy,rem;
+	cout<<"enter a number: ";
+	cin>>num;
+	copy=num;
+	while(num>0){
+		rem=num%10;
+		num=num/10;
+		ans=ans*10+rem;
+	}
+	if(ans==copy){
+		cout<<"is palindrome: "<<ans;
+		return 0;
+	}
+	else{
+		cout<<"is not a palindrome: "<<ans;
+		return 0;
+	}
+	return 0;
+}
