e1b53b3a07fc74528c9d3648a4e6afee6dbd6161
mdMERAJ77 <mmd200975@gmail.com>
2025-11-08 10:42:46 +0530

    leetcode69 sqrt(x)

---
+++ DSA_COMEBACK_MODE/Day10/squareOf2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 1, num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if(num < 1){
+        cout << "Only positive numbers allowed";
+        return 0;
+    }
+
+    while(i * i <= num){ //safe for overflow: (i<=num/i)
+        i++;
+    }
+
+    cout << "Approximate integer square root: " << (i - 1);
+    return 0;
+}
+
