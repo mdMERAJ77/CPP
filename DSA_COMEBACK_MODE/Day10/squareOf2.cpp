823f239dc054ab95fd0ed8f3a88172f9396ff9fa
mdMERAJ77 <mmd200975@gmail.com>
2025-11-08 11:51:59 +0530

    square of 2 or sqrt(x)

--- DSA_COMEBACK_MODE/Day10/squareOf2.cpp
+++ DSA_COMEBACK_MODE/Day10/squareOf2.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main(){
-    int i = 1, num;
-    cout << "Enter a number: ";
-    cin >> num;
+    int i =1,num;
+    cout <<"Enter a number: ";
+    cin>>num;
 
-    if(num < 1){
-        cout << "Only positive numbers allowed";
+    if(num<1){
+        cout <<"Only positive numbers allowed";
         return 0;
     }
 
-    while(i * i <= num){ //safe for overflow: (i<=num/i)
+    while(i*i<=num){ //safe for overflow: (i<=num/i)
         i++;
     }
 
-    cout << "Approximate integer square root: " << (i - 1);
+    cout <<"Approximate integer square root:"<<(i - 1);
     return 0;
 }
 
