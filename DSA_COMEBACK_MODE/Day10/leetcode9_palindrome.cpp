76eeb33db48412a4ea8f7c3a588c3f95f8f0fe2b
mdMERAJ77 <mmd200975@gmail.com>
2025-11-08 10:57:19 +0530

    leetcode 9 palindrome

--- DSA_COMEBACK_MODE/Day10/leetcode9_palindrome.cpp
+++ DSA_COMEBACK_MODE/Day10/leetcode9_palindrome.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
+#include <climits>
 using namespace std;
 int main(){
 	int num,copy,rem;
-	long ans=0; // to handle integer overflow
+	//long ans=0; // second way to handle integer overflow
+	int ans=0;
 	cout<<"enter a number: ";
 	cin>>num;
 	copy=num;
 	while(num>0){
 		rem=num%10;
 		num=num/10;
+		if(ans>INT_MAX/10){ // handle integer overflow
+			cout<<"is not a palindrome";
+			return 0;
+		}
 		ans=ans*10+rem;
 	}
 	if(ans==copy){
