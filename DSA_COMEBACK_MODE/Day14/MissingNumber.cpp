016b955f41866a1f5f64beb84b061c489b712347
mdMERAJ77 <mmd200975@gmail.com>
2025-11-11 14:52:21 +0530

    find missing number from array

--- DSA_COMEBACK_MODE/Day14/MissingNumber.cpp
+++ DSA_COMEBACK_MODE/Day14/MissingNumber.cpp
@@ -4,7 +4,7 @@ int main(){
 	int arr[5]={1,2,3,5,6};
 	int sum=0,indexSum=0;
 	//sum of natural number upto n=6;
-	sum=6*(6+1)/2;
+	sum=6*(6+1)/2; //formula of sum: n*(n+1)/2
 	for(int i=0; i<5; i++){
 		indexSum=indexSum+arr[i];
 	}
