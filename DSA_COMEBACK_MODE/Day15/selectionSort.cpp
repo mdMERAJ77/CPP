54cf405732975ee26635dad0a88358a4c31c23b7
mdMERAJ77 <mmd200975@gmail.com>
2025-11-17 00:13:45 +0530

    selection sort

---
+++ DSA_COMEBACK_MODE/Day15/selectionSort.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[5]={4,2,1,3,0};
+	for(int i=0;i<4;i++){
+		int index=i;
+		for(int j=i+1; j<=4; j++){
+			if(arr[j]<arr[index]){
+				index=j;
+			}
+		}
+		swap(arr[i],arr[index]);
+	}
+	for(int i=0; i<5; i++){
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
